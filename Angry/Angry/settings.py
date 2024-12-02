fb2859d37672fdbc4e13f61d0529f831eecdf969
Alchosamurai <alchosan.py@gmail.com>
2024-12-02 15:20:46 +0600

    redis settings

--- Angry/Angry/settings.py
+++ Angry/Angry/settings.py
@@ -101,6 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',  # Укажите адрес вашего Redis-сервера
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
