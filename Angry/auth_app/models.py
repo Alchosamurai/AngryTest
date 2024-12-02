e0ddad37e5fe4b3bfc89b25e4f72703bb11007e3
Alchosamurai <alchosan.py@gmail.com>
2024-12-02 15:18:57 +0600

    create CustomUser & TelegramUser models

--- Angry/auth_app/models.py
+++ Angry/auth_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from telegram_app.models import TelegramUser
 # Create your models here.
+class CustomUser(User):
+    telegram_user = models.ForeignKey(TelegramUser, on_delete=models.SET_NULL, null=True, blank=True)
--- Angry/telegram_app/models.py
+++ Angry/telegram_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class TelegramUser(models.Model):
+    chat_id = models.IntegerField()
+    username = models.CharField(max_length=100, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.username if self.username else f"User {self.chat_id}"
