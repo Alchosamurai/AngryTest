a8acfed1ea52e802d3a9ba8bd70c59629df5e898
Alchosamurai <alchosan.py@gmail.com>
2024-12-02 15:23:24 +0600

    Изменил наследования кастомного юзера с User на AbstractUser

--- Angry/auth_app/models.py
+++ Angry/auth_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from telegram_app.models import TelegramUser
 # Create your models here.
-class CustomUser(User):
+class CustomUser(AbstractUser):
     telegram_user = models.ForeignKey(TelegramUser, on_delete=models.SET_NULL, null=True, blank=True)
