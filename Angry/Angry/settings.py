d1bd1c4ea567ff7fd9b8a4e1628c1e1e194723dd
Alchosamurai <alchosan.py@gmail.com>
2024-12-02 15:43:00 +0600

    стандартный механизм авторизации джанго

--- Angry/Angry/settings.py
+++ Angry/Angry/settings.py
@@ -133,3 +133,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'auth_app.CustomUser'
+
+
+LOGIN_REDIRECT_URL = '/home' 
+LOGOUT_REDIRECT_URL = '/accounts/login/'  
--- Angry/auth_app/views.py
+++ Angry/auth_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])  
+            user.save()
+            messages.success(request, 'Your account has been created! You can now log in.')
+            return redirect('login')  
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
